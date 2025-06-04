2b3ed79859ed1e23269c9afc128a701bdd53538c
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-04 15:31:21 +0300

    feat: integrate React Query and add React Query Devtools to main application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="text-3xl text-primary">Hello World</div>;
+	return <div className="text-primary m-5 flex text-3xl">Hello World</div>;
 }
 
 export default App;
