df44766bdf2b778c14f77281a0a05c17ac95a87b
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-04 18:11:49 +0300

    feat: wrap RouterProvider with ThemeProvider for theme context integration

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 
+import { Toaster } from "./components/ui/sonner.tsx";
+import { ThemeProvider } from "./contexts/themeContext.tsx";
 import "./index.css";
 import { Router } from "./router.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<QueryClientProvider client={new QueryClient()}>
-			<RouterProvider router={Router} />
-			{/* React Query Devtools for debugging */}
-			<ReactQueryDevtools initialIsOpen={false} />
+			<ThemeProvider>
+				<RouterProvider router={Router} />
+				<Toaster position="top-center" richColors />
+				{/* React Query Devtools for debugging */}
+				<ReactQueryDevtools initialIsOpen={false} />
+			</ThemeProvider>
 		</QueryClientProvider>
 	</StrictMode>
 );
