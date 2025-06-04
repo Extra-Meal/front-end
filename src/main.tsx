1e9b7be1d90aa16784bbd6b24bc0e51403364ede
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-04 16:20:05 +0300

    feat: add react-router for routing and create router configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
 
-import App from "./App.tsx";
 import "./index.css";
+import { Router } from "./router.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<QueryClientProvider client={new QueryClient()}>
-			<App />
+			<RouterProvider router={Router} />
+			{/* React Query Devtools for debugging */}
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	</StrictMode>
