64ab2ee7294b520f90bdf19a9d06a9b64f4bd591
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-04 19:18:17 +0300

    refactor: disable no-explicit-any rule for TypeScript in useApi and api files

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { type UseMutationOptions, type UseQueryOptions, useMutation, useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router";
 
