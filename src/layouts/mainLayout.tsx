61cdb398fba3683d8cfb3a0ecb3c6998a20cffb0
Maha Elshazly <mahaelshazly32@gmail.com>
2025-06-13 18:57:35 +0300

    add components to layout

--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router";
 
+
 import ThemeToggler from "@/components/themeToggler";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 export default function MainLayout() {
   return (
     <div className="flex min-h-screen flex-col">
-      <nav>
-        <h1>Navbar</h1>
-        <ThemeToggler />
-      </nav>
+      <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
-      <footer>Footer</footer>
+      <Footer />
     </div>
   );
 }
