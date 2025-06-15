63da77880b0e411c995df95e3c8b6f2c7288c261
Maha Elshazly <mahaelshazly32@gmail.com>
2025-06-15 06:51:53 +0300

    for better ui until get the logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -39,7 +39,7 @@ export default function Navbar() {
   ];
 
   return (
-    <div className="navbar bg-background shadow-md fixed top-0 left-0 right-0 z-500">
+    <div className="navbar bg-background fixed top-0 right-0 left-0 z-500 shadow-md">
       <div className="container">
         {/* large screen */}
         {/*----- design 1  ---------*/}
@@ -172,34 +172,43 @@ export default function Navbar() {
         </nav> */}
 
         {/*--------- design 3 -------*/}
-        <nav className="hidden justify-between lg:flex ">
+        <nav className="hidden justify-between lg:flex">
           <div className="tabs flex items-center">
             <NavigationMenu>
-              <NavigationMenuList>
-                {menu.map((item) => renderMenuItem(item, currentPath))}
-              </NavigationMenuList>
+              <NavigationMenuList>{menu.map((item) => renderMenuItem(item, currentPath))}</NavigationMenuList>
             </NavigationMenu>
           </div>
-           <div className="logo-icon ">
-            <Link to={"/"} className="max-w-36 ">
-              <svg width="229.00000000000006" height="46.21764852589558" viewBox="0 0 369.89473684210526 74.33905457489806" class="looka-1j8o68f">
+          <div className="logo-icon">
+            <Link to={"/"} className="max-w-36">
+              <svg
+                width="229.00000000000006"
+                height="46.21764852589558"
+                viewBox="0 0 369.89473684210526 74.33905457489806"
+                class="looka-1j8o68f"
+              >
                 <defs id="SvgjsDefs6025"></defs>
-                
-                  <g id="SvgjsG6027" featurekey="VomJeJ-0" transform="matrix(2.6532381166578745,0,0,2.6532381166578745,109.61208544197541,4.211393087413061)" fill="#af3e3e"><path d="M14.94 9.74 l-5.1 6.1 l-5.06 -6.1 l0 10.26 l-2.14 0 l0 -12.82 l-1.74 -2.1 l2.82 0 l6.1 7.32 l6.18 -7.32 l2.82 0 l-1.74 2.1 l0 12.82 l-2.14 0 l0 -10.26 z M24.099999999999998 14.02 l0 3.8 l9.2 0 l0 2.18 l-11.38 0 l0 -14.92 l10.5 0 l0 2.2 l-8.32 0 l0 4.52 q0.62 -0.36 1.38 -0.48 q0.56 -0.1 1.44 -0.1 l3.26 0 l0 2.22 l-3.26 0 q-0.82 0 -1.44 0.1 q-0.76 0.14 -1.38 0.48 z M41.46 6.66 l-0.82 -1.84 l2.4 0 l7.18 15.18 l-2.44 0 l-0.98 -2.18 l-4.54 0 q-1.28 0 -2.3 0.24 q-0.9 0.22 -1.5 0.58 q-0.54 0.32 -0.72 0.66 l-0.38 0.7 l-2.36 0 z M43.06 15.620000000000001 l2.76 0 l-3.26 -6.3 l-3.3 7.14 q0.44 -0.38 1.34 -0.6 q1.02 -0.24 2.46 -0.24 z M63.480000000000004 20 l-10.46 0 l0 -14.92 l2.18 0 l0 12.74 l8.28 0 l0 2.18 z M68.22 20 l-2.14 0 l0 -14.92 l2.14 0 l0 14.92 z M74.8 14.02 l0 5.98 l-2.18 0 l0 -14.92 l10.5 0 l0 2.2 l-8.32 0 l0 4.52 q0.62 -0.36 1.38 -0.48 q0.56 -0.1 1.44 -0.1 l3.26 0 l0 2.22 l-3.26 0 q-0.82 0 -1.44 0.1 q-0.76 0.14 -1.38 0.48 z M91.46000000000001 11.64 q1.34 -1.4 2.42 -3.12 t1.72 -3.44 l2.5 0 q-1.02 2.38 -2.48 4.56 q-1.32 1.98 -3.08 3.9 l0 6.46 l-2.16 0 l0 -6.46 q-1.76 -1.92 -3.08 -3.9 q-1.46 -2.18 -2.48 -4.56 l2.5 0 q0.64 1.72 1.72 3.44 t2.42 3.12 z"></path></g></svg>
+
+                <g
+                  id="SvgjsG6027"
+                  featurekey="VomJeJ-0"
+                  transform="matrix(2.6532381166578745,0,0,2.6532381166578745,109.61208544197541,4.211393087413061)"
+                  fill="#af3e3e"
+                >
+                  <path d="M14.94 9.74 l-5.1 6.1 l-5.06 -6.1 l0 10.26 l-2.14 0 l0 -12.82 l-1.74 -2.1 l2.82 0 l6.1 7.32 l6.18 -7.32 l2.82 0 l-1.74 2.1 l0 12.82 l-2.14 0 l0 -10.26 z M24.099999999999998 14.02 l0 3.8 l9.2 0 l0 2.18 l-11.38 0 l0 -14.92 l10.5 0 l0 2.2 l-8.32 0 l0 4.52 q0.62 -0.36 1.38 -0.48 q0.56 -0.1 1.44 -0.1 l3.26 0 l0 2.22 l-3.26 0 q-0.82 0 -1.44 0.1 q-0.76 0.14 -1.38 0.48 z M41.46 6.66 l-0.82 -1.84 l2.4 0 l7.18 15.18 l-2.44 0 l-0.98 -2.18 l-4.54 0 q-1.28 0 -2.3 0.24 q-0.9 0.22 -1.5 0.58 q-0.54 0.32 -0.72 0.66 l-0.38 0.7 l-2.36 0 z M43.06 15.620000000000001 l2.76 0 l-3.26 -6.3 l-3.3 7.14 q0.44 -0.38 1.34 -0.6 q1.02 -0.24 2.46 -0.24 z M63.480000000000004 20 l-10.46 0 l0 -14.92 l2.18 0 l0 12.74 l8.28 0 l0 2.18 z M68.22 20 l-2.14 0 l0 -14.92 l2.14 0 l0 14.92 z M74.8 14.02 l0 5.98 l-2.18 0 l0 -14.92 l10.5 0 l0 2.2 l-8.32 0 l0 4.52 q0.62 -0.36 1.38 -0.48 q0.56 -0.1 1.44 -0.1 l3.26 0 l0 2.22 l-3.26 0 q-0.82 0 -1.44 0.1 q-0.76 0.14 -1.38 0.48 z M91.46000000000001 11.64 q1.34 -1.4 2.42 -3.12 t1.72 -3.44 l2.5 0 q-1.02 2.38 -2.48 4.56 q-1.32 1.98 -3.08 3.9 l0 6.46 l-2.16 0 l0 -6.46 q-1.76 -1.92 -3.08 -3.9 q-1.46 -2.18 -2.48 -4.56 l2.5 0 q0.64 1.72 1.72 3.44 t2.42 3.12 z"></path>
+                </g>
+              </svg>
             </Link>
           </div>
           <div className="icons flex items-center gap-4">
             <ThemeToggler />
-            <ShoppingBasket className="text-primary "/>
+            <ShoppingBasket className="text-primary" />
             <Link to="/login">
-              <Button className="relative  group overflow-hidden rounded-3xl px-8 py-2 border-l-3 border-r-3  border-primary bg-transparent text-primary hover:text-white transition-all duration-300">
+              <Button className="group border-primary text-primary relative overflow-hidden rounded-3xl border-r-3 border-l-3 bg-transparent px-8 py-2 transition-all duration-300 hover:text-white">
                 <span className="relative z-10">Login</span>
-                <span className="absolute inset-0 z-0 before-animation"></span>
+                <span className="before-animation absolute inset-0 z-0"></span>
               </Button>
             </Link>
-
           </div>
-          
         </nav>
 
         {/* mobile  */}
