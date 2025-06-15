e7c9696c1efd9bdc6e8e63abe148e9e5cca7a7eb
Maha Elshazly <mahaelshazly32@gmail.com>
2025-06-15 03:37:13 +0300

    add areas component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,12 @@
 import AboutUs from "@/components/home/aboutus";
 import Hero from "@/components/home/hero.tsx";
-
+import Areas from "@/components/home/areas";
 function Home() {
   return (
     <div>
       <Hero />
       <AboutUs />
+      <Areas />
     </div>
   );
 }
