f762cc0e487fcbd0a809f75fdf5cb86af4d01247
kareem172 <kareemkhalaf1722@gmail.com>
2025-06-15 23:48:55 +0300

    add type definitions for circletype module

---
+++ src/types/circletype.d.ts
@@ -0,0 +1,8 @@
+declare module 'circletype' {
+  export default class CircleType {
+    constructor(el: HTMLElement);
+    radius(r: number): this;
+    dir(d: number): this;
+    forceHeight(b: boolean): this;
+  }
+}
