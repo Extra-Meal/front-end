f4cfddd5b089b0723269ddb577bf55a29eea3959
Maha Elshazly <mahaelshazly32@gmail.com>
2025-06-15 08:25:38 +0300

    add sections to home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,14 +2,20 @@ import AboutUs from "@/components/home/aboutus";
 import Areas from "@/components/home/areas";
 import Firstorder from "@/components/home/firstorder";
 import Hero from "@/components/home/hero.tsx";
+import Ingredients from "@/components/home/ingredients";
+import Meals from "@/components/home/meals";
+import SmartAi from "@/components/home/smartAi";
 
 function Home() {
   return (
     <div>
       <Hero />
       <AboutUs />
+      <Meals />
       <Firstorder />
+      <Ingredients />
       <Areas />
+      <SmartAi />
     </div>
   );
 }
