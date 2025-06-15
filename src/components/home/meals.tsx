bfe6634ad05952c5994f95b27ae077ca99b0a2fb
Maha Elshazly <mahaelshazly32@gmail.com>
2025-06-15 10:01:38 +0300

    remove badge import

--- src/components/home/meals.tsx
+++ src/components/home/meals.tsx
@@ -2,8 +2,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import { Badge } from "@/components/ui/badge";
-
 import { Button } from "../ui/button";
 
 interface Meal {
