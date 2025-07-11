ed1d5803fec963981441f057a898b1f2840a6489
kareem172 <kareemkhalaf1722@gmail.com>
2025-07-11 11:24:46 +0300

    feat: add CartButton and WishListButton components for cart and wishlist functionality

---
+++ src/components/wishListButton.tsx
@@ -0,0 +1,49 @@
+import type { VariantProps } from "class-variance-authority";
+import { Heart } from "lucide-react";
+import React from "react";
+
+import { useWishList } from "@/hooks/useWishList";
+import { cn } from "@/lib/utils";
+
+import { Button, buttonVariants } from "./ui/button";
+
+type WishListButtonProps = React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+    children?: React.ReactNode;
+    altText?: string;
+    productId: string;
+    quantity?: number;
+  };
+export default function WishListButton({
+  children,
+  productId,
+  quantity,
+  altText,
+  ...ButtonProps
+}: WishListButtonProps) {
+  const { addProductToWishlist, removeProductFromWishlist, isProductInWishlist, isAdding, isRemoving } = useWishList();
+  const isInWishList = isProductInWishlist(productId);
+
+  function handleCLick() {
+    if (isInWishList) {
+      removeProductFromWishlist(productId);
+    } else {
+      addProductToWishlist(productId);
+    }
+  }
+  return (
+    <Button
+      onClick={handleCLick}
+      className={cn(buttonVariants({ variant: "secondary", size: "icon" }), "")}
+      {...ButtonProps}
+    >
+      {isInWishList ? (
+        <Heart className={isRemoving ? "animate-pulse" : ""} fill="var(--color-secondary-foreground)" />
+      ) : (
+        <Heart className={isAdding ? "animate-pulse" : ""} />
+      )}
+      {!isInWishList ? children : altText}
+    </Button>
+  );
+}
