6c97026cf05223a70d5bdf310ed703e008cdfd15
Eslam-Elwey <eslam.elwey@gmail.com>
2025-07-11 19:29:33 +0300

    display meals pagination

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,7 +1,16 @@
-import MealCard from "@/components/mealCard";
-import { useGetData } from "@/hooks/useApi";
+import { useSearchParams } from "react-router-dom";
+import { useGetDataWithParams } from "@/hooks/useApi";
 import type { APISuccess } from "@/types/api.type";
 import type { Meal } from "@/types/meal.type";
+import MealCard from "@/components/mealCard";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 type Response = {
   meals: Array<Meal>;
@@ -12,15 +21,63 @@ type Response = {
     totalPages: number;
   };
 };
+
 export default function MenuPage() {
-  const { data, isPending } = useGetData<APISuccess<Response>>("/meal", {});
-  const { meals } = data?.data || {};
+  const [params, setParams] = useSearchParams();
+  const page = Number(params.get("page") || 1);
+
+
+  const { data, isPending } = useGetDataWithParams<APISuccess<Response>>("/products/kits");
+
+  const totalPages = data?.data?.pagination.totalPages || 1;
+  const meals = data?.data?.products || [];
+
+  const goToPage = (pageNumber: number) => {
+    const newParams = new URLSearchParams(params);
+    newParams.set("page", String(pageNumber));
+    setParams(newParams);
+  };
+
   if (isPending) {
     return <div className="container">Loading...</div>;
   }
+
   return (
-    <div className="container grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-5">
-      {meals?.map((meal) => <MealCard key={meal._id} meal={meal} />)}
+    <div className="container space-y-8">
+
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-5">
+        {meals.map((meal) => (
+          <MealCard key={meal._id} meal={meal} />
+        ))}
+      </div>
+
+      {totalPages > 1 && (
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious onClick={() => goToPage(Math.max(1, page - 1))} />
+            </PaginationItem>
+
+            {[...Array(totalPages)].map((_, index) => {
+              const current = index + 1;
+              return (
+                <PaginationItem key={current}>
+                  <PaginationLink
+                    isActive={current === page}
+                    onClick={() => goToPage(current)}
+                  >
+                    {current}
+                  </PaginationLink>
+                </PaginationItem>
+              );
+            })}
+
+            <PaginationItem>
+              <PaginationNext onClick={() => goToPage(Math.min(totalPages, page + 1))} />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      )}
     </div>
   );
 }
