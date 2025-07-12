ff3160626d3543452bf2feda8078a0792aedd350
Maha Elshazly <mahaelshazly32@gmail.com>
2025-07-12 13:26:40 +0300

    category card

--- src/components/Categories/CategoryCard.tsx
+++ src/components/Categories/CategoryCard.tsx
@@ -1,22 +1,15 @@
-import { Coffee } from "lucide-react";
-
-import areas from "../../assets/areas.jfif";
-
-function CategoryCard() {
+function CategoryCard({ category }: any) {
   return (
     <div>
-      <div className="service-item relative mb-30 overflow-hidden rounded-2xl">
-        <img src={areas} alt="" className="w-full" />
+      <div className="service-item relative overflow-hidden rounded-2xl">
+        <img src={category.thumbnail} alt="" className="w-full" />
         <div className="content absolute bottom-0 left-0 z-10 flex w-full flex-col items-center gap-3 pb-[50px] text-center transition-all duration-400">
-          <Coffee className="text-white transition-all duration-400" size={40} />
           <div className="title">
-            <a href="#" className="text-4xl font-bold text-white">
-              <span>-</span> Coffee <span>-</span>
+            <a href="#" className="text-3xl font-bold text-white">
+              {category.name}
             </a>
           </div>
-          <p className="text-white transition-all duration-400">
-            Alienum phaedrum torquatos nec eu, vis detraxit periculis ex.
-          </p>
+          <p className="text-sm text-white transition-all duration-400">{category.description.slice(0, 70)}.</p>
         </div>
       </div>
     </div>
