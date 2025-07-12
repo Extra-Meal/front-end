d22f9f986228587d98f5994c8eda9443e0fa93b2
Maha Elshazly <mahaelshazly32@gmail.com>
2025-07-12 13:27:54 +0300

    categories page

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -1,15 +1,26 @@
 import CategoryCard from "@/components/Categories/CategoryCard";
 import HeroSubPage from "@/components/heroSubPage";
+import { useGetData } from "@/hooks/useApi";
+import type { Category } from "@/types/category.type";
 
 function Categories() {
+  const { data, isLoading, error, isError } = useGetData<{
+    success: boolean;
+    message: string;
+    data: Category[];
+  }>("http://localhost:3000/api/category");
+  console.log(data);
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error?.data.message}</p>;
+
   return (
     <div>
       <HeroSubPage title="Categories" />
       <div className="container">
-        <div className="grid grid-cols-5 gap-5">
-          <CategoryCard />
-          <CategoryCard />
-          <CategoryCard />
+        <div className="container">
+          <div className="grid grid-cols-4 gap-x-6 gap-y-7">
+            {data?.data?.map((category) => <CategoryCard key={category.name} category={category} />)}
+          </div>
         </div>
       </div>
     </div>
