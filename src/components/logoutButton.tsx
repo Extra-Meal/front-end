13dfbe28cd97598ac050dc9cdeb93a1da1a1fc87
kareem172 <kareemkhalaf1722@gmail.com>
2025-07-12 21:11:29 +0300

    refactor: simplify LogoutButton component for improved readability and styling

--- src/components/logoutButton.tsx
+++ src/components/logoutButton.tsx
@@ -1,3 +1,5 @@
+import { LogOut } from "lucide-react";
+
 import { useAuth } from "@/hooks/useAuth";
 
 import { Button } from "./ui/button";
@@ -5,12 +7,9 @@ import { Button } from "./ui/button";
 export default function LogoutButton() {
   const { logout } = useAuth();
   return (
-    <Button
-      onClick={logout}
-      className="group border-primary text-primary relative overflow-hidden rounded-3xl border-r-3 border-l-3 bg-transparent px-8 py-2 transition-all duration-300 hover:text-white"
-    >
-      <span className="relative z-10">Logout</span>
-      <span className="before-animation absolute inset-0 z-0"></span>
+    <Button onClick={logout} className="w-full">
+      <LogOut className="text-primary-foreground" />
+      Logout
     </Button>
   );
 }
