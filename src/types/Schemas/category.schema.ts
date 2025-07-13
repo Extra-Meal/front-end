11927d5452488e3969407e4829e98f3c9b61ac84
kareem172 <kareemkhalaf1722@gmail.com>
2025-07-13 04:49:09 +0300

    fix: simplify thumbnail validation in category schema and update sanitized schema

--- src/types/Schemas/category.schema.ts
+++ src/types/Schemas/category.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const categorySchema = z.object({
   _id: z.string(),
   name: z.string().min(1, "Name is required"),
-  thumbnail: z.union([z.string().url("Thumbnail must be a valid URL"), z.instanceof(File), z.null()]),
+  thumbnail: z.string().url("Thumbnail must be a valid URL"),
   description: z
     .string({
       required_error: "Description is required",
@@ -12,7 +12,7 @@ export const categorySchema = z.object({
     .min(1, "Description is required"),
 });
 
-export const categorySanitizedSchema = categorySchema.omit({ thumbnail: true }).extend({
+export const categorySanitizedSchema = categorySchema.omit({ thumbnail: true, _id: true }).extend({
   thumbnail: z.union([z.string().url(), z.instanceof(File), z.null()]),
 });
 
